72d7eba48ed405fe2580f227bbc705e8afd565f6
Nathan Rathbun <naterathbun@gmail.com>
2017-03-23 14:45:15 -0400

    Got it working!

--- GroupPaint/Scripts/script.js
+++ GroupPaint/Scripts/script.js
@@ -5,6 +5,11 @@ window.onload = function () {
 
 $(document).ready(function () {
 
+    $(".brushColor").click(function () {
+        $(".selectedColor").removeClass("selectedColor");
+        $(this).addClass("selectedColor");
+    });
+
     var mouseButtonIsDown = false;
 
     $(document).mousedown(function () {
@@ -17,12 +22,69 @@ $(document).ready(function () {
 
     $(".pixel").mouseover(function () {
         if (mouseButtonIsDown) {
-            $(this).css({ background: "#FFFFFF" });
+            $(this).css("background-color", $(".selectedColor").css("background-color"));
         }
     });
 
     $('.pixel').mousedown(function () {
-        $(this).css({ background: "#FFFFFF" });
+        $(this).css("background-color", $(".selectedColor").css("background-color"));
     });
 
+
+
+    /*
+    // Create the web socket connection object
+    var connection = $.connection("/togglesocket");
+
+
+    // Attach an event handler to execute code
+    // when a message is received
+    connection.received(function (data) {
+        var dataObject = JSON.parse(data);
+
+        if (dataObject.type == "cellUpdate") {
+            updateCell(dataObject.row, dataObject.col, dataObject.colorValue);
+        } else {
+            updateTable(dataObject);
+        }
+
+    });
+
+    connection.start();
+
+    $("td").click(function () {
+
+        var rowIndex = $(this).attr("data-row");
+        var colIndex = $(this).attr("data-col");
+        var colorValue = $(".selectedColor").css("background-color");
+
+        var message = {
+            type: "cellUpdate",
+            row: rowIndex,
+            col: colIndex,
+            colorValue: colorValue
+        };
+        connection.send(message);
+    });*/
+    
 });
+
+
+function updateCell(row, column, colorValue) {
+    var cell = $(`td[data-row='${row}']`).filter(`td[data-col='${column}']`);
+    cell.css("background-color", colorValue);
+    cell.removeClass("default");
+}
+
+function updateTable(data) {
+    for (var rowIndex = 0; rowIndex < data.mapData.length; rowIndex++) {
+        for (var colIndex = 0; colIndex < data.mapData[rowIndex].length; colIndex++) {
+                        
+            var colorValue = data.mapData[rowIndex][colIndex];
+            if(colorValue != null){
+                updateCell(rowIndex, colIndex, colorValue);
+            }
+            
+        }
+    }
+}
