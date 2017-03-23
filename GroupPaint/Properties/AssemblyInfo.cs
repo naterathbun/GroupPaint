fa963708f927f08fef6d62ab30cfb9d06e0e6ba0
Nathan Rathbun <naterathbun@gmail.com>
2017-03-23 13:14:10 -0400

    Rebuilt as MVC app

--- GroupPaint/Properties/AssemblyInfo.cs
+++ GroupPaint/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("072306c8-449d-44da-a236-e4b08c426e6e")]
+[assembly: Guid("4375800a-c7c8-46e5-a09d-27c65c165231")]
 
 // Version information for an assembly consists of the following four values:
 //
